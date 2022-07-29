7c9f7a0913ec7955e6805c5eb817830b349766c7
Ian Budiman <ianpaul.budiman@gmail.com>
2022-07-29 13:16:45 +0800

    Add all files

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "AustraliaEast"
+
+  tags = {
+    Environment = "CloudResumeChallenge"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "australiaeast"
+  resource_group_name = azurerm_resource_group.rg.name
+}
